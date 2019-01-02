2f514542a24bb521fa45c75133f3028ea7984058
Bruno Farias <brunofarias1@gmail.com>
2019-01-02 18:16:07 +0200

    Created BuildControls component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burguer/Burger'
+import BuildControls from '../../components/Burguer/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
   state = {
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     )
   }
