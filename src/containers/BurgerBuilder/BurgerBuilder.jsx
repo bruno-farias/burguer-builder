b01e97211efb291e33771b13999d0544286cc6f9
Bruno Farias <brunofarias1@gmail.com>
2019-01-03 14:39:21 +0200

    hide/show modal based on amount of ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   }
 
   updatePurchaseState (ingredients) {
@@ -71,6 +72,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients)
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true})
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -80,7 +85,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -89,7 +94,8 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disable={disabledInfo}
           purchasable={this.state.purchasable}
-          price={this.state.totalPrice} />
+          price={this.state.totalPrice}
+          ordered={this.purchaseHandler}/>
       </Aux>
     )
   }
